830b203f3b8f9ee1a582ccffa7678687928583fe
Atticus <atticus@pondfamily.net>
2018-07-21 15:42:59 -0600

    Initial commit.

---
+++ Assets/Scripts/JeepMotion.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class JeepMotion : MonoBehaviour
+{
+
+    float speed;
+    public VirtualJoystick joystick;
+
+    void Start()
+    {
+        speed = .1f;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = joystick.Horizontal();
+        float moveVertical = joystick.Veriticle();
+        if (moveVertical != 0 || moveHorizontal != 0)
+        {
+            gameObject.transform.position = new Vector2(gameObject.transform.position.x + moveHorizontal * speed,
+                                                        gameObject.transform.position.y + moveVertical * speed);
+            transform.eulerAngles = new Vector3(0.0f, 0.0f, (Mathf.Atan2(moveVertical, moveHorizontal) * (180 / Mathf.PI)) + 180);
+        }
+    }
+
+}
